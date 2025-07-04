9a595b1bc85ee53d551af617414d6779799cff56
Alisson alves <alissonalves64@hotmail.com>
2025-07-03 23:45:23 -0300

    feat: add Logo component with styles and update App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Container } from './components/Container';
 import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
+
 import './styles/theme.css';
 import './styles/global.css';
 
@@ -7,11 +9,11 @@ export function App() {
   return (
     <>
       <Container>
-        <Heading>Test</Heading>
+        <Logo />
       </Container>
 
       <Container>
-        <Heading>2</Heading>
+        <Heading>Menu</Heading>
       </Container>
     </>
   );
