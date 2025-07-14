3b30aacd6cb51bdcd924f504d469d11316bf7ed5
alisson <alissonalves64@hotmail.com>
2025-07-14 20:38:14 -0300

    feat: update NotFound and AboutPomodoro pages with new content and structure; fix GenericHtml styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Home } from './pages/Home';
+// import { Home } from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css';
+import { NotFound } from './pages/NotFound';
+import { AboutPomodoro } from './pages/AboutPomodoro';
 
 export function App() {
-  return (
-      <Home />
-  );
+  return <NotFound />;
 }
