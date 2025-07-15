390614a7a26d722daf97776557abb70a41b04569
alisson <alissonalves64@hotmail.com>
2025-07-14 22:09:34 -0300

    feat: update App component to render Home page; add TaskModel and TaskStateModel types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-// import { Home } from './pages/Home';
+import { Home } from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css';
-import { NotFound } from './pages/NotFound';
-import { AboutPomodoro } from './pages/AboutPomodoro';
+// import { NotFound } from './pages/NotFound';
+// import { AboutPomodoro } from './pages/AboutPomodoro';
 
 export function App() {
-  return <NotFound />;
+  return <Home />;
 }
